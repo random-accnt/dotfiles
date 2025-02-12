0d88314ec396550fe4d6633129fec35b5d6276ea
randomAccnt <jiri.capsky@seznam.cz>
2025-02-12 13:50:10 +0100

    add exclude option to sync.sh, delete lazy-lock.json

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -av --delete ~/.config/nvim/ ~/dotfiles/nvim/
+rsync -av --delete --exclude "lazy-lock.json" ~/.config/nvim/ ~/dotfiles/nvim/
 rsync -av --delete ~/.bash_profile ~/dotfiles/.bash_profile
 rsync -av --delete ~/.bashrc ~/dotfiles/.bashrc
 rsync -av --delete ~/.tmux/tmux.conf ~/dotfiles/.tmux/tmux.conf
