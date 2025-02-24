cdf06bbd71f7b7f54a7a2afe2d12486f1df907de
random-accnt <jiri.capsky@seznam.cz>
2025-02-24 09:59:18 +0100

    add i3 and polybar

--- sync.sh
+++ sync.sh
@@ -4,3 +4,5 @@ rsync -av --delete ~/.bash_profile ~/dotfiles/.bash_profile
 rsync -av --delete ~/.bashrc ~/dotfiles/.bashrc
 rsync -av --delete ~/.tmux/tmux.conf ~/dotfiles/.tmux/tmux.conf
 rsync -av --delete ~/.zshrc ~/dotfiles/.zshrc
+rsync -av --delete ~/.config/i3/ ~/dotfiles/i3/
+rsync -av --delete --exclude "polybar.log" ~/.config/polybar/ ~/dotfiles/polybar/
