9e9161d0da31df4d385754c12672f7fc1783a6dd
random-accnt <jiri.capsky@seznam.cz>
2025-02-24 13:03:46 +0100

    moved tmux.conf into .config/tmux

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 rsync -av --delete --exclude "lazy-lock.json" ~/.config/nvim/ ~/dotfiles/nvim/
 rsync -av --delete ~/.bash_profile ~/dotfiles/.bash_profile
 rsync -av --delete ~/.bashrc ~/dotfiles/.bashrc
-rsync -av --delete ~/.tmux/tmux.conf ~/dotfiles/.tmux/tmux.conf
+rsync -av --delete ~/.config/tmux/ ~/dotfiles/tmux/
 rsync -av --delete ~/.zshrc ~/dotfiles/.zshrc
 rsync -av --delete ~/.config/i3/ ~/dotfiles/i3/
 rsync -av --delete --exclude "polybar.log" ~/.config/polybar/ ~/dotfiles/polybar/
